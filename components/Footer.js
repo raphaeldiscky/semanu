73a31f3b6c785571904f42d207b77f65f79891b7
Raphael Discky <zundria.putra@gmail.com>
2022-07-13 23:20:19 +0700

    feat: add footer component

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+      <div className='my-5 items-center justify-center text-center'>
+        <img className='mx-auto mb-2 h-[50px] w-[50px]' src='/logo-ugm.png' />
+        <h1>
+          Dibuat oleh <span className='font-bold'>KKN UGM SEMANU 2022</span>
+        </h1>
+      </div>
+    )
+  }
+  
+  export default Footer
+  
