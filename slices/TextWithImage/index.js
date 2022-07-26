96a9c8dfb7baa81d5f569c40507959c4db895657
Raphael Discky <zundria.putra@gmail.com>
2022-07-26 20:34:21 +0700

    fix: left image with text layout

--- slices/TextWithImage/index.js
+++ slices/TextWithImage/index.js
@@ -1,28 +1,43 @@
-import * as prismicH from "@prismicio/helpers";
-import { PrismicRichText } from "@prismicio/react";
-import { PrismicNextImage } from "@prismicio/next";
+import * as prismicH from '@prismicio/helpers'
+import { PrismicRichText } from '@prismicio/react'
+import { PrismicNextImage } from '@prismicio/next'
 
-import { Bounded } from "../../components/Bounded";
+import { Bounded } from '../../components/Bounded'
 
 const TextWithImage = ({ slice }) => {
-  const image = slice.primary.image;
+  const image = slice.primary.image
 
-  return (
-    <Bounded as="section" className="bg-white">
-      <div className="grid grid-cols-1 items-center gap-8 md:grid-cols-2">
+  return slice.variation === 'leftImage' ? (
+    <Bounded as='section' className='bg-white'>
+      <div className='flex items-center justify-center -md:flex-col-reverse'>
+        <div className='w-full md:mr-[16px] md:w-1/2 -md:mt-[32px]'>
+          {prismicH.isFilled.image(image) && (
+            <div className='bg-gray-100'>
+              <PrismicNextImage field={image} layout='responsive' />
+            </div>
+          )}
+        </div>
+        <div className='w-full md:ml-[16px] md:w-1/2'>
+          <PrismicRichText field={slice.primary.text} />
+        </div>
+      </div>
+    </Bounded>
+  ) : (
+    <Bounded as='section' className='bg-white'>
+      <div className='grid grid-cols-1 items-center gap-8 md:grid-cols-2'>
         <div>
           <PrismicRichText field={slice.primary.text} />
         </div>
         <div>
           {prismicH.isFilled.image(image) && (
-            <div className="bg-gray-100">
-              <PrismicNextImage field={image} layout="responsive" />
+            <div className='bg-gray-100'>
+              <PrismicNextImage field={image} layout='responsive' />
             </div>
           )}
         </div>
       </div>
     </Bounded>
-  );
-};
+  )
+}
 
-export default TextWithImage;
+export default TextWithImage
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  content: ["./pages/**/*.js", "./components/**/*.js", "./slices/**/*.js"],
+  content: ['./pages/**/*.js', './components/**/*.js', './slices/**/*.js'],
   theme: {
     fontFamily: {
-      sans: 'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      sans: 'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
     },
-    extend: {},
+    extend: {
+      screens: {
+        '-2xl': { raw: '(max-width: 1535px)' },
+        '-xl': { raw: '(max-width: 1279px)' },
+        '-lg': { raw: '(max-width: 1023px)' },
+        '-md': { raw: '(max-width: 767px)' },
+        '-sm': { raw: '(max-width: 639px)' }
+      }
+    }
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
-};
+  plugins: [require('@tailwindcss/aspect-ratio')]
+}
