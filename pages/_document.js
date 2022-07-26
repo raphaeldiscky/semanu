6f1711dc0944813a17202082f22b67642b000941
Raphael Discky <zundria.putra@gmail.com>
2022-07-26 13:37:03 +0700

    feat: add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap'
+          rel='stylesheet'
         />
+        <link rel='shortcut icon' href='/logo-ugm.png' />
       </Head>
-      <body className="overflow-x-hidden antialiased">
+      <body className='overflow-x-hidden antialiased'>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
